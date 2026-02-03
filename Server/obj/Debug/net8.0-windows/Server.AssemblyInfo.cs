9340031a6bb5ffd40c3622c7c0fd80b7711ac5bd
Aleksa Ljujić <aleksa.ljujic2@gmail.com>
2026-02-03 05:43:18 +0100

    removed some files

--- Server/obj/Debug/net8.0-windows/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0-windows/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecb47f9cd5646f1c249c4fc6166f6891faa7f888")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
